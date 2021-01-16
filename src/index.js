6423b8918bc222723af4758106fee59af7cc2734
Stasya Geleysha <s.geleisha@gmail.com>
2021-01-16 19:05:37 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    let count = 0;
+    let arr = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == "(" || str[i] == "{" || str[i] == "[" || str[i] == "|") {           
+            if (str[i] == "|") {
+                arr.push(str[i]);          
+            }
+            count++;
+
+        } else {
+            count--;
+        }
+
+        if (count < 0) {
+            break;
+        }
+    }
+
+    if (arr.length % 2 == 0) {
+        count = count - 2 * (arr.length / 2);
+
+    } else {
+        count--;
+    }
+
+    if (count != 0) {
+        return false;
+    } else {
+        return true; //
+    }
+
+};
